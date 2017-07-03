8800039ee35abcf46ce5e34ec2319a407bdc7ed7
jeff.thompson <jeffery.thompson@wedoresi.com>
2017-07-03 10:17:52 -0400

    Added NetStandard csproj

--- ICD.Common.Permissions/IAction.cs
+++ ICD.Common.Permissions/IAction.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Crestron.SimplSharp;
-
-namespace ICD.Common.Permissions
+﻿namespace ICD.Common.Permissions
 {
 	public interface IAction
 	{
