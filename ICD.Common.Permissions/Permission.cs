de6462e85cf1a74f794ebdef06a851327b5ed87d
jeff.thompson <jeffery.thompson@wedoresi.com>
2017-07-03 14:21:36 -0400

    Added methods to parse from xml

--- ICD.Common.Permissions/Permission.cs
+++ ICD.Common.Permissions/Permission.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections.Generic;
+using ICD.Common.Utils.Xml;
 
 namespace ICD.Common.Permissions
 {
 	public class Permission
 	{
+
+		private const string ACTION_ELEMENT = "Action";
+		private const string ROLE_ELEMENT = "Role";
+		private const string ROLES_ELEMENT = ROLE_ELEMENT + "s";
+
 		public IAction Action { get; set; }
 
 		public IEnumerable<string> Roles { get; set; }
+
+		public static Permission FromXml(string xml)
+		{
+			Permission permission = new Permission();
+			permission.Action = Permissions.Action.FromString(XmlUtils.TryReadChildElementContentAsString(xml, ACTION_ELEMENT));
+			string roles;
+			if (XmlUtils.TryGetChildElementAsString(xml, ROLES_ELEMENT, out roles))
+				permission.Roles = GetRolesFromXml(roles);
+			return permission;
+		}
+
+		private static IEnumerable<string> GetRolesFromXml(string xml)
+		{
+			foreach (var role in XmlUtils.GetChildElementsAsString(xml, ROLE_ELEMENT))
+				yield return XmlUtils.ReadElementContent(role);
+		}
 	}
 }
