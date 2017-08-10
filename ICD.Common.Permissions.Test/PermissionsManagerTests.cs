ff2953056793747a786bfbc1282d8826f4e139c7
Chris Cameron <chris.cameron@wedoresi.com>
2017-08-10 16:22:08 -0400

    Tidying namespace

--- ICD.Common.Permissions.Test/PermissionsManagerTests.cs
+++ ICD.Common.Permissions.Test/PermissionsManagerTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
-namespace ICD.Common.Permissions.Test_SimplSharp
+namespace ICD.Common.Permissions.Test
 {
 	[TestFixture]
 	public class PermissionsManagerTests
