16c48cc44c38644a68f3737343e6e5fe5cdd7438
Rashod Davis <davisrashod@gmail.com>
2018-05-18 16:11:54 -0400

    chore: updating submodule

--- ICD.Common.Permissions.Tests/PermissionsManagerTests.cs
+++ ICD.Common.Permissions.Tests/PermissionsManagerTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
-namespace ICD.Common.Permissions.Test
+namespace ICD.Common.Permissions.Tests
 {
 	[TestFixture]
 	public sealed class PermissionsManagerTests
