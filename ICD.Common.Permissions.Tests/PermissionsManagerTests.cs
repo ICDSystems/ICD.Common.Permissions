d1bbf85b174e4d3af80af23dd85e1e9cbafaeae6
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-30 16:12:44 -0400

    refactor: Tidying

--- ICD.Common.Permissions.Tests/PermissionsManagerTests.cs
+++ ICD.Common.Permissions.Tests/PermissionsManagerTests.cs
@@ -9,55 +9,55 @@ namespace ICD.Common.Permissions.Tests
 	public sealed class PermissionsManagerTests
 	{
 		private const string ACTION = "TestPermissable";
-		private PermissionsManager manager;
-		private Object testObj;
+		private PermissionsManager m_Manager;
+		private Object m_TestObj;
 
 		[SetUp]
 		public void Init()
 		{
-			testObj = new object();
-			manager = new PermissionsManager();
-			manager.SetDefaultPermissions(CreateDefaultPermissions());
-			manager.SetObjectPermissions(testObj, CreateObjectPermissions());
-			manager.DefaultRoles = new [] {"DefaultRole"};
+			m_TestObj = new object();
+			m_Manager = new PermissionsManager();
+			m_Manager.SetDefaultPermissions(CreateDefaultPermissions());
+			m_Manager.SetObjectPermissions(m_TestObj, CreateObjectPermissions());
+			m_Manager.DefaultRoles = new [] {"DefaultRole"};
 		}
 
 		private static IEnumerable<Permission> CreateDefaultPermissions()
 		{
-			yield return new Permission() {Permissable = Permissable.FromString(ACTION), Roles = new [] {"TestRole"}};
-			yield return new Permission() {Permissable = Permissable.FromString("FallbackPermissable"), Roles = new[] {"FallbackRole"}};
+			yield return new Permission {Permissable = Permissable.FromString(ACTION), Roles = new [] {"TestRole"}};
+			yield return new Permission {Permissable = Permissable.FromString("FallbackPermissable"), Roles = new[] {"FallbackRole"}};
 		}
 
 		private static IEnumerable<Permission> CreateObjectPermissions()
 		{
-			yield return new Permission() {Permissable = Permissable.FromString(ACTION), Roles = new [] {"TestObjectRole"}};
+			yield return new Permission {Permissable = Permissable.FromString(ACTION), Roles = new [] {"TestObjectRole"}};
 		}
 
 		[Test]
 		public void GetRoles_DefaultPermissions()
 		{
-			var roles = manager.GetRoles(Permissable.FromString(ACTION)).ToList();
+			var roles = m_Manager.GetRoles(Permissable.FromString(ACTION)).ToList();
 			Assert.Contains("TestRole", roles);
 		}
 
 		[Test]
 		public void GetRoles_WithObject_ObjectPermissions()
 		{
-			var roles = manager.GetRoles(Permissable.FromString(ACTION), testObj).ToList();
+			var roles = m_Manager.GetRoles(Permissable.FromString(ACTION), m_TestObj).ToList();
 			Assert.Contains("TestObjectRole", roles);
 		}
 
 		[Test]
 		public void GetRoles_FallbackToDefaultRoles()
 		{
-			var roles = manager.GetRoles(Permissable.FromString("DifferentPermissable")).ToList();
+			var roles = m_Manager.GetRoles(Permissable.FromString("DifferentPermissable")).ToList();
 			Assert.Contains("DefaultRole", roles);
 		}
 
 		[Test]
 		public void GetRoles_WithObject_FallbackToDefaultPermissions()
 		{
-			var roles = manager.GetRoles(Permissable.FromString("FallbackPermissable"), testObj).ToList();
+			var roles = m_Manager.GetRoles(Permissable.FromString("FallbackPermissable"), m_TestObj).ToList();
 			Assert.Contains("FallbackRole", roles);
 		}
 	}
