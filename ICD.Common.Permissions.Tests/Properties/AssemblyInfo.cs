8417881d6ef71de0ab7133c3c8859807a6c32934
Chris Cameron <chris.cameron@icdsystems.com>
2022-01-05 16:27:18 -0500

    chore: Updating AssemblyInfo year

--- ICD.Common.Permissions.Tests/Properties/AssemblyInfo.cs
+++ ICD.Common.Permissions.Tests/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Common.Permissions.Test")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Permissions.Test")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
 [assembly: AssemblyVersion("4.0.0.0")]
 
--- ICD.Common.Permissions/Properties/AssemblyInfo.cs
+++ ICD.Common.Permissions/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Common.Permissions")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Permissions")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
 [assembly: AssemblyVersion("4.0.0.0")]
