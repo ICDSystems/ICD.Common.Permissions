4299b5bbdc1327d65fa5da06b9d6eaa7b5959e50
Drew Tingen <drew.tingen@icdsystems.com>
2023-03-22 08:10:25 -0400

    chore: Update changelog, increment assembly patch version

--- ICD.Common.Permissions.Tests/Properties/AssemblyInfo.cs
+++ ICD.Common.Permissions.Tests/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Permissions.Test")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2023")]
-[assembly: AssemblyVersion("4.0.1.0")]
+[assembly: AssemblyVersion("4.0.2.0")]
 
--- ICD.Common.Permissions/Properties/AssemblyInfo.cs
+++ ICD.Common.Permissions/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Permissions")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2023")]
-[assembly: AssemblyVersion("4.0.1.0")]
+[assembly: AssemblyVersion("4.0.2.0")]
